86e437ff99a55f25866eafbe7e255d9313aa1d01
Kas Buunk <kasbuunk@icloud.com>
2026-06-06 14:34:26 +0200

    feat: interpret, define, substitute, deref

--- interpreter.hs
+++ interpreter.hs
@@ -4,7 +4,7 @@ import Data.Map qualified as Map
 
 data Expr = Atom Atom | Cons (Expr) (Expr)
 
-data Atom = Boolean Bool | Nand | Symbol Symbol
+data Atom = Boolean Bool | Nand | Symbol Symbol | Define | Apply
 
 type Env = Map.Map Symbol (Expr)
 
@@ -21,11 +21,27 @@ nand (Atom (Boolean b1)) (Atom (Boolean b2)) = Atom (Boolean (not (b1 && b2)))
 interpreter :: EnvStack -> Expr -> EnvStack
 interpreter envs expr = envs
 
+interpret :: Env -> Expr -> (Env, Expr)
+interpret env (Atom (Boolean b)) = (env, eval env (Atom (Boolean b)))
+interpret env (Atom (Symbol s)) = (env, eval env (Atom (Symbol s)))
+interpret env (Cons (Atom Nand) (Cons e1 e2)) = (env, eval env (Cons (Atom Nand) (Cons e1 e2)))
+interpret env (Cons (Atom Define) (Cons (Atom (Symbol s)) e)) = (define env s e, e)
+interpret env (Cons (Atom Apply) (Cons (e) (Atom (Symbol s)))) = (env, substitute s (deref env s) e)
+interpret env (Cons e1 e2) = (env, cons (snd (interpret env e1)) (snd (interpret env e2)))
+
 eval :: Env -> Expr -> Expr
-eval _ (Atom (Boolean b)) = Atom (Boolean b)
+eval env (Atom (Boolean b)) = Atom (Boolean b)
 eval env (Atom (Symbol s)) = Map.findWithDefault (Atom (Symbol s)) s env
 eval env (Cons (Atom Nand) (Cons e1 e2)) = nand (eval env e1) (eval env e2)
-eval env (Cons e1 e2) = cons (eval env e1) (eval env e2)
+
+define :: Env -> Symbol -> Expr -> Env
+define env s e = Map.insert s e env
+
+deref :: Env -> Symbol -> Expr
+deref env s = Map.findWithDefault (Atom (Symbol s)) s env
+
+substitute :: Symbol -> Expr -> Expr -> Expr
+substitute s value (Atom (Symbol replaceMe)) = if s == replaceMe then value else Atom (Symbol replaceMe)
 
 sampleExpr :: Expr
 sampleExpr = Atom (Boolean True)
